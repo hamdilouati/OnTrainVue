3e0e049901b95f98dd4dfd3b4715f6fc2abf2c15
Hamdi Louati <louati.hamd@gmail.com>
2023-05-03 23:24:49 +0200

    step 5

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 import './assets/main.css'
 
-const app = createApp(App)
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    cache,
+    uri: 'http://127.0.0.1:8000/graphql',
+})
+const app = createApp({
+    setup () {
+        provide(DefaultApolloClient, apolloClient)
+    },
+    render: () => h(App),
+})
 
 app.use(createPinia())
 app.use(router)
